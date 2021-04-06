c72f32b2f05aa0ecd2c474a078f0853124ce9407
merylschoice22 <kekrieg.digital@gmail.com>
2021-04-06 20:02:56 +0200

    Week 3 in class work

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,25 +1,45 @@
-function setAlarm() {}
+document.querySelector('#set').addEventListener('click', setAlarm)
+let alarmSetInput = document.querySelector('#alarmSet').valueAsNumber;
+let timeRemaining = document.querySelector('#timeRemaining');
+let currentTime = alarmSetInput;
+
+function setAlarm() {
+  timeRemaining.innerText = `Time Remaining: ${alarmSetInput}`;
+
+  setInterval(function (num) {
+    timeRemaining.innerText = `Time Remaining: ${alarmSetInput}`;
+    currentTime -= 1
+  }, 1000)
+}
+
+if (currentTime === 0) {
+  playAlarm();
+}
+
+const stopAlarmBtn = document.querySelector('#stop');
+stopAlarmBtn.addEventListener('click', pauseAlarm)
+
 
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+// var audio = new Audio("alarmsound.mp3");
 
-function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
+// function setup() {
+//   document.getElementById("set").addEventListener("click", () => {
+//     setAlarm();
+//   });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
-}
+//   document.getElementById("stop").addEventListener("click", () => {
+//     pauseAlarm();
+//   });
+// }
 
-function playAlarm() {
-  audio.play();
-}
+// function playAlarm() {
+//   audio.play();
+// }
 
-function pauseAlarm() {
-  audio.pause();
-}
+// function pauseAlarm() {
+//   audio.pause();
+// }
 
-window.onload = setup;
+// window.onload = setup;
--- week-3/InClass/D-fetch/exercise.js
+++ week-3/InClass/D-fetch/exercise.js
@@ -10,3 +10,4 @@ user latitude and longitude.
 
 ================
 */
+
