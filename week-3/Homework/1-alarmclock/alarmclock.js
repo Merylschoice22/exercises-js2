c03cf01f8879e9b77f1b97d4ec6c24e91144d8d9
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-14 15:15:13 +0200

    Alarm clock exercise with blinking text

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,28 +1,115 @@
-document.querySelector('#set').addEventListener('click', setAlarm)
-let alarmSetInput = document.querySelector('#alarmSet').valueAsNumber;
-let timeRemaining = document.querySelector('#timeRemaining');
-let currentTime = alarmSetInput;
+// const createPauseBtn = () => {
+//   const pauseAlarmBtn = document.createElement("button");
+//   pauseAlarmBtn.innerText = "Pause Alarm";
+//   pauseAlarmBtn.setAttribute(type, button);
+//   pauseAlarmBtn.setAttribute(id, "pause");
+//   document.body.querySelector(".centre").appendChild(pauseAlarmBtn);
+// };
+// createPauseBtn();
+
+document.querySelector("#set").addEventListener("click", setAlarm);
+
+let alarmSetInput = document.querySelector("#alarmSet");
+
+let timeRemaining = document.querySelector("#timeRemaining");
+
+let tickTock = null;
+
+/*** SET ALARM BUTTON FUNCTION ***/
 
 function setAlarm() {
-  timeRemaining.innerText = `Time Remaining: ${alarmSetInput}`;
+  document.body.style.backgroundColor = "";
+  timeRemaining.style.color = "";
+  let alarmInputValue = alarmSetInput.value;
+  tickTock = setInterval(() => {
+    let minutes = Math.floor(alarmInputValue / 60);
+    if (minutes < 10) {
+      minutes = minutes.toString().padStart(2, "0");
+    }
+    let seconds = alarmInputValue % 60;
+    if (seconds < 10) {
+      seconds = seconds.toString().padStart(2, "0");
+    }
+    let time = minutes + ":" + seconds;
+    alarmSetInput.value = "";
+    timeRemaining.innerText = `Time Remaining: ${time}`;
 
-  setInterval(function (num) {
-    timeRemaining.innerText = `Time Remaining: ${alarmSetInput}`;
-    currentTime -= 1
-  }, 1000)
+    console.log(alarmInputValue, time);
+    alarmInputValue -= 1;
+
+    if (alarmInputValue < 0) {
+      alarm();
+    }
+  }, 1000);
 }
 
-if (currentTime === 0) {
+/**** ALARM FINISHES FUNCTION ****/
+
+const alarm = () => {
   playAlarm();
-}
+  clearInterval(tickTock);
+  document.body.style.backgroundColor = "red";
+  alarmFinishedBlinking();
+};
 
-const stopAlarmBtn = document.querySelector('#stop');
-stopAlarmBtn.addEventListener('click', pauseAlarm)
+/** ALARM TEXT BLINKING **/
+let textDisplay = true;
+let blinkingText = null;
 
+const alarmFinishedBlinking = () => {
+  blinkingText = setInterval(() => {
+    if (textDisplay) {
+      console.log("Hide text");
+      timeRemaining.style.color = "red";
+
+      textDisplay = false;
+    } else {
+      console.log("Show text");
+      timeRemaining.style.color = "";
+
+      textDisplay = true;
+    }
+    console.log("BLINK");
+  }, 500);
+};
+
+/** PAUSE ALARM BUTTON FUNCTION **/
+const pauseAlarm = () => {
+  audio.pause();
+  if (alarmInputValue > 0) {
+    blinkingTime = setInterval(() => {
+      if (textDisplay) {
+        console.log("Hide text");
+        timeRemaining.style.color = "red";
+
+        textDisplay = false;
+      } else {
+        console.log("Show text");
+        timeRemaining.style.color = "";
+
+        textDisplay = true;
+      }
+      console.log("BLINK");
+    }, 500);
+  }
+
+  clearInterval(tickTock);
+};
+
+/** STOP ALARM BUTTON FUNCTION **/
+
+const stopAlarm = () => {
+  clearInterval(tickTock);
+  audio.pause();
+  clearInterval(blinkingText);
+};
+
+const stopAlarmBtn = document.querySelector("#stop");
+stopAlarmBtn.addEventListener("click", stopAlarm);
 
 // DO NOT EDIT BELOW HERE
 
-// var audio = new Audio("alarmsound.mp3");
+var audio = new Audio("alarmsound.mp3");
 
 // function setup() {
 //   document.getElementById("set").addEventListener("click", () => {
@@ -34,9 +121,9 @@ stopAlarmBtn.addEventListener('click', pauseAlarm)
 //   });
 // }
 
-// function playAlarm() {
-//   audio.play();
-// }
+function playAlarm() {
+  audio.play();
+}
 
 // function pauseAlarm() {
 //   audio.pause();
