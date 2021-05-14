50dfd14bcee656ac1db019c1c28dc79fbfc35a94
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-14 20:08:32 +0200

    Finished movie project

--- week-3/InClass/B-movies/exercise.js
+++ week-3/InClass/B-movies/exercise.js
@@ -42,14 +42,20 @@ function showMovies() {
     movies.forEach((movie) => {
       let paragraph = document.createElement("p");
       paragraph.innerText = movie.title + " - " + movie.director;
-      console.log(movie.title + " - " + movie.director);
       movieSection.appendChild(paragraph);
       moviesNumber.innerText = movies.length;
     });
   }, 1000);
 }
-showMovies(); // create a new movie object for your favorite movie
+showMovies();
 
+// create a new movie object for your favorite movie
+let myMovies = {
+  title: "Moulin Rouge",
+  director: "Baz Luhrmann",
+  type: "musical",
+  haveWatched: true,
+};
 /* Task 2
 Amend your function above to only show movies after 1 second. Remember to use setTimeout to achieve that*/
 
@@ -59,20 +65,18 @@ Create a new function called "addMovie"
 - it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
 Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
 How many movies can you see on your page?
-*/ function addMovie() {
-  let movie = {};
-  movie.title = "Moulin Rouge";
-  movie.director = "Baz Luhrmann";
-  movie.type = "musical";
-  movie.haveWatched = true;
-  movies.push(movie);
-  let p = document.createElement("p");
-  p.innerText = movie.title + " - " + movie.director;
-  movieSection.appendChild(p);
-  moviesNumber.innerText = movies.length;
-  console.log(`Added.`);
+*/
+
+function addMovie(newMovie) {
+  setTimeout(() => {
+    movies.push(newMovie);
+    let p = document.createElement("p");
+    p.innerText = newMovie.title + " - " + newMovie.director;
+    movieSection.appendChild(p);
+    moviesNumber.innerText = movies.length;
+  }, 2000);
 }
-setTimeout(addMovie, 2000);
+addMovie(myMovies);
 
 /* Task 3
  Can you make sure the new movie you just added is showing on the screen? 
@@ -88,25 +92,20 @@ When the button is clicked
 TIP: Use the functions you created on tasks 1-3 */
 // create addMovies function
 
-const createForm = () => {
-  let form = document.createElement("form");
-  let title = document.createElement("input");
-  let director = document.createElement("input");
-  let type = document.createElement("input");
-  let haveWatched = document.createElement("input");
-  title.setAttribute("type", "text");
-  director.setAttribute("type", "text");
-  type.setAttribute("type", "text");
-  haveWatched.setAttribute("type", "text");
-  movieSection.appendChild(form);
-  form.appendChild(title);
-  form.appendChild(director);
-  form.appendChild(type);
-  form.appendChild(haveWatched);
-  let saveBtn = document.createElement("button");
-  saveBtn.innerHTML = "type = button";
-  saveBtn.innerText = "Save";
-  movieSection.appendChild(saveBtn);
+const addNewMovies = () => {
+  let title = document.getElementById("title");
+  let director = document.getElementById("director");
+  let type = document.getElementById("type");
+  let haveWatched = document.getElementById("haveWatched");
+  let inputMovie = {
+    title: title.value,
+    director: director.value,
+    type: type.value,
+    haveWatched: haveWatched.value,
+  };
+  console.log(inputMovie);
+  addMovie(inputMovie);
 };
 
-createForm();
+let button = document.getElementById("addMovieBtn");
+button.addEventListener("click", addNewMovies);
