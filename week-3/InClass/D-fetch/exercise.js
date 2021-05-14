df42545613977c1e9b7b7b60d1fa95eaa711e812
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-14 22:13:22 +0200

    Updated Fetch exercise. Validation doesn't work

--- week-3/InClass/D-fetch/exercise.js
+++ week-3/InClass/D-fetch/exercise.js
@@ -11,3 +11,44 @@ user latitude and longitude.
 ================
 */
 
+/*** PRINT WELCOME MESSAGE ***/
+let h3 = document.createElement("h3");
+let h2 = document.createElement("h2");
+h2.innerText = "Input coordinates to locate a city";
+h3.innerText = "The city temperature will be shown here";
+document.body.appendChild(h2);
+document.body.appendChild(h3);
+
+/*** VARIABLES ***/
+let lat = document.getElementById("lat").value.trim();
+let lon = document.getElementById("lon").value.trim();
+
+/*** INPUT FETCH ***/
+document.querySelector("form").addEventListener("submit", (getTemperature) => {
+  getTemperature.preventDefault();
+  fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
+    .then((response) => response.json())
+    .then((temperatures) => {
+      console.log(temperatures);
+      console.log(lat, lon);
+      h3.innerText = `The current temperature there is ${temperatures.main.temp} degrees Celsius`;
+      h2.innerText = `You have input coordinates for ${temperatures.name.toUpperCase()}`;
+      document.body.appendChild(h2);
+      document.body.appendChild(h3);
+    })
+    .catch((error) => console.log(`Error: ${error}`));
+});
+
+/*** FORM VALIDATION DOESN'T WORK***/
+
+const validateForm = () => {
+  if (isNaN(parseFloat(lat))) {
+    // let warning = document.createElement("h1");
+    // let label = document.querySelector("label");
+    // label.appendChild(warning);
+    // warning.innerText = "Enter valid coordinates";
+    alert("Enter valid coordinates!");
+    return;
+  }
+};
+document.getElementById("getBtn").addEventListener("click", validateForm);
