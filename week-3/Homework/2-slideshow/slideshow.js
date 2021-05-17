db9e4de209e8cd3f444104ec1b92235bfaeb2b0e
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-18 01:16:45 +0200

    First commit of Slideshow. Created IMG folder, wrote basic HTML structure, and began JS code with functions for forward and backward buttons.

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,1 +1,46 @@
-// Write your code here
+const images = [
+  "./img/img1.jpg",
+  "./img/img2.jpg",
+  "./img/img3.jpg",
+  "./img/img4.jpg",
+  "./img/img5.jpg",
+];
+
+let img = document.querySelector("img");
+
+const showNext = (imgPool) => {
+  const currentImage = document.querySelector("img").getAttribute("src");
+  console.log("Next photo plz");
+  console.log(currentImage);
+  let currentIndex = imgPool.indexOf(currentImage);
+  console.log(imgPool.indexOf(currentImage));
+  currentIndex === imgPool.length - 1
+    ? (currentIndex = 0)
+    : (currentIndex += 1);
+
+  console.log("New index" + currentIndex);
+
+  img.setAttribute("src", imgPool[currentIndex]);
+};
+
+const showPrevious = (imgPool) => {
+  const currentImage = document.querySelector("img").getAttribute("src");
+  console.log(currentImage);
+  let currentIndex = imgPool.indexOf(currentImage);
+  console.log(imgPool.indexOf(currentImage));
+  currentIndex === 0
+    ? (currentIndex = imgPool.length - 1)
+    : (currentIndex -= 1);
+
+  console.log("New index" + currentIndex);
+
+  img.setAttribute("src", imgPool[currentIndex]);
+};
+
+document
+  .querySelector("#backward")
+  .addEventListener("click", () => showPrevious(images));
+
+document
+  .querySelector("#forward")
+  .addEventListener("click", () => showNext(images));
