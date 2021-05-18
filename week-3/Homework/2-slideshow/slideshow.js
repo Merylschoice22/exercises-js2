78f6bf260315901f21734ad52c08ee32129afc99
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-19 01:57:23 +0200

    Added more images. Changed default image. Added auto buttons and stop with accompanying functions

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -4,37 +4,56 @@ const images = [
   "./img/img3.jpg",
   "./img/img4.jpg",
   "./img/img5.jpg",
+  "./img/img6.jpg",
+  "./img/img7.jpg",
+  "./img/img8.jpg",
+  "./img/img9.jpg",
+  "./img/img10.jpg",
+  "./img/img11.jpg",
 ];
 
 let img = document.querySelector("img");
+let run = true;
 
 const showNext = (imgPool) => {
   const currentImage = document.querySelector("img").getAttribute("src");
-  console.log("Next photo plz");
-  console.log(currentImage);
   let currentIndex = imgPool.indexOf(currentImage);
-  console.log(imgPool.indexOf(currentImage));
+  console.log("Previous photo index" + imgPool.indexOf(currentImage));
   currentIndex === imgPool.length - 1
     ? (currentIndex = 0)
     : (currentIndex += 1);
-
   console.log("New index" + currentIndex);
-
   img.setAttribute("src", imgPool[currentIndex]);
+  run = false;
+};
+
+const showNextAuto = (imgPool) => {
+  go = setInterval(() => showNext(imgPool), 1000);
+  run = true;
 };
 
 const showPrevious = (imgPool) => {
   const currentImage = document.querySelector("img").getAttribute("src");
-  console.log(currentImage);
   let currentIndex = imgPool.indexOf(currentImage);
-  console.log(imgPool.indexOf(currentImage));
+  console.log("Previous photo index" + imgPool.indexOf(currentImage));
   currentIndex === 0
     ? (currentIndex = imgPool.length - 1)
     : (currentIndex -= 1);
-
   console.log("New index" + currentIndex);
-
   img.setAttribute("src", imgPool[currentIndex]);
+  run = false;
+};
+
+const showPreviousAuto = (imgPool) => {
+  go = setInterval(() => showPrevious(imgPool), 1000);
+  run = true;
+};
+
+const stop = () => {
+  if ((run = true)) {
+    clearInterval(go);
+    run = false;
+  }
 };
 
 document
@@ -44,3 +63,13 @@ document
 document
   .querySelector("#forward")
   .addEventListener("click", () => showNext(images));
+
+document
+  .querySelector("#autoBack")
+  .addEventListener("click", () => showPreviousAuto(images));
+
+document
+  .querySelector("#autoForward")
+  .addEventListener("click", () => showNextAuto(images));
+
+document.querySelector("#stop").addEventListener("click", stop);
