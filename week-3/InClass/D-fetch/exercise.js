c6905db599dc4af9c930cb5de365cb8ae7e32092
merylschoice22 <kekrieg.digital@gmail.com>
2021-05-20 23:39:59 +0200

    Finished validation function

--- week-3/InClass/D-fetch/exercise.js
+++ week-3/InClass/D-fetch/exercise.js
@@ -12,41 +12,53 @@ user latitude and longitude.
 */
 
 /*** PRINT WELCOME MESSAGE ***/
-let h3 = document.createElement("h3");
-let h2 = document.createElement("h2");
+const h3 = document.createElement("h3");
+const h2 = document.createElement("h2");
 h2.innerText = "Input coordinates to locate a city";
 h3.innerText = "The city temperature will be shown here";
 document.body.appendChild(h2);
 document.body.appendChild(h3);
+const getBtn = document.querySelector("#getBtn");
+getBtn.setAttribute("disabled", true);
 
 /*** VARIABLES ***/
-let lat = "";
-let lon = "";
-console.log("lat" + lat, "lon" + lon);
+// let lat = "";
+// let lon = "";
+// console.log("BEFORE lat " + lat, " lon " + lon);
 /*** INPUT FETCH ***/
-document.querySelector("form").addEventListener("submit", (getTemperature) => {
-  getTemperature.preventDefault();
-  lat = document.getElementById("lat").value;
-  lon = document.getElementById("lon").value;
-  console.log("lat" + lat, "lon" + lon);
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  if (lat == "" || lon == "") {
-    alert("Enter valid coordinates!");
-  }
-  fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
-    .then((response) => response.json())
-    .then((temperatures) => {
-      console.log("Coordinates: " + lat, lon);
-      h3.innerText = `The current temperature there is ${temperatures.main.temp} degrees Celsius`;
-      h2.innerText = `You have input coordinates for ${temperatures.name.toUpperCase()}`;
-      document.body.appendChild(h2);
-      document.body.appendChild(h3);
-    })
-    .catch((error) => console.log(`Error: ${error}`));
+  let lat = document.getElementById("lat").value;
+  let lon = document.getElementById("lon").value;
+  console.log("IN THE FORM lat " + lat, " lon " + lon);
+  (isValid(lat, lon) &&
+    fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`)
+      .then((response) => response.json())
+      .then((temperatures) => {
+        console.log("Coordinates THEN: " + lat, lon);
+        h3.innerText = `The current temperature there is ${temperatures.main.temp} degrees Celsius`;
+        h2.innerText = `You have input coordinates for ${temperatures.name.toUpperCase()}`;
+        document.body.appendChild(h2);
+        document.body.appendChild(h3);
+      })
+      .catch((error) => console.log(`Error: ${error}`))) ||
+    alert("Invalid inputs!");
 });
 
 /*** FORM VALIDATION***/
 
 // const validateForm = (lat, lon) => {
+//   console.log("VALIDATION INPUTS lat " + lat, " lon " + lon);
+
+//   if (lat == "" || lon == "") {
+//     alert("One or more of your inputs are empty!");
+//   }
+
 // };
-// document.getElementById("getBtn").addEventListener("click", validateForm);
+
+const isValid = (lat, lon) => {
+  console.log("VALIDATION INPUTS lat " + lat, " lon " + lon);
+
+  return parseFloat(lat) && parseFloat(lon);
+};
